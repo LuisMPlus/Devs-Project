bb73f2a83fd730604908d6385dd8b5c1541a5e39
LuisMPlus <lx147812345@gmail.com>
2026-05-13 02:06:45 -0300

    eliminar lo anterior a la migracion a next ts

--- server/src/routes/subjectRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getSubjectsByCareer } from '../controllers/subjectController'
-
-const router = Router()
-
-// Generic route: GET /subjects/:careerSlug
-router.get('/:careerSlug', getSubjectsByCareer)
-
-export default router
